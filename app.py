a738fb307ccd7efe49aa7da167e538049dd01cec
Programmer-Develops <shachipandya29@gmail.com>
2025-05-13 18:05:12 +0530

    setup

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 # import matplotlib.pyplot as plt
-import plotly.express as px
+# import plotly.express as px
 import csv
 
 st.write("""
