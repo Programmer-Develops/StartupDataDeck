47a31984b44a569ed8df6525e4790d7a6539e25e
Programmer-Develops <shachipandya29@gmail.com>
2025-05-13 17:55:42 +0530

    setup

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+import csv
+
+st.write("""
+# My first app
+Hello *world!*
+""")
+ 
+df = pd.read_csv("investments_VC.csv")
+st.line_chart(df)
